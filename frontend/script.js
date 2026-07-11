fff001e733cf10acd13feb7fa8b031f045b7117d
bhumikumari935-wq <bhumikumari935@gmail.com>
2026-07-11 22:40:23 +0530

    Fix login redirect

--- frontend/script.js
+++ frontend/script.js
@@ -21,7 +21,7 @@ if (loginForm) {
 
             if (data.message === "Login Successful ✅") {
                 alert(data.message);
-                window.location.href = "product.html";
+                window.location.href = "./product.html";
             } else {
                 alert(data.message);
             }
